e14951012fc6c97072c6b9637e77f9727dd2a2dd
Alexandre APPPERCE <aapperce@uptoo.fr>
2022-11-21 14:05:25 +0100

    add react

--- src/component/icons/format_bold.js
+++ src/component/icons/format_bold.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function Bold ({ className = 'fill-current' }) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
