57169c1b400b3b61dd2e1c1fa16ace6a4f8a4007
nsaintemarie <nsaintemarie@gmail.com>
2024-02-13 18:10:49 +0100

    test

--- src/component/index.js
+++ src/component/index.js
@@ -56,6 +56,7 @@ export default function MyEditor({
   id,
   ...props
 }) {
+  
   // Editeur
   const editor = useMemo(() => withLinks(withTags(withMentions(withReact(withHistory(createEditor()))))), [])
 
